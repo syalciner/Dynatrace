d5120603d78a53fd59229ea300760b8675a63e0d
syalciner <serkanyalciner@gmail.com>
2024-12-12 12:16:09 +0300

    Add files via upload
    chmod +x disable_env.py
    chmod +x enable_env.py
    
    *** Linux sistemde crontab dosyasını düzenleyin:
    crontab -e
    
    **** Aşağıdaki satırları ekleyin:
    # Her gün saat 22:00'de, Cumartesi ve Pazar hariç environment'ları DISABLED yap
    0 22 * * 1-5 /usr/bin/python3 /path/to/disable_env.py
    
    # Her gün saat 07:00'de, Cumartesi ve Pazar hariç environment'ları ENABLED yap
    0 7 * * 1-5 /usr/bin/python3 /path/to/enable_env.py
    
    *****Cron jobların başarıyla eklendiğinden emin olmak için:
    crontab -l

---
+++ enable_env.py
@@ -0,0 +1,45 @@
+import requests
+import urllib3
+
+# SSL uyarılarını bastır
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# Dynatrace API bilgileri
+CLUSTER_TOKEN = "Api-Token xxxxxx"
+BASE_URL = "https://xxxxxxx/api/cluster/v2/environments"
+ENV_IDS = [
+    "xxxENV_TOKENxxxxx",  # Test Env 1
+    "dxxxENV_TOKENxxxxx"   # Test Env 2
+]
+
+def update_environment_state(env_ids, cluster_token, base_url, state):
+    headers = {
+        'Authorization': cluster_token,
+        'Content-Type': 'application/json',
+    }
+
+    payload = {
+        "state": state
+    }
+
+    for env_id in env_ids:
+        try:
+            url = f"{base_url}/{env_id}"
+            response = requests.put(url, headers=headers, json=payload, verify=False)
+            if response.status_code in [200, 204]:
+                print(f"Environment '{env_id}' başarıyla {state} durumuna getirildi.")
+            else:
+                print(f"Hata: Environment '{env_id}' durumuna getirilemedi. Status Code: {response.status_code}")
+                print(f"Yanıt: {response.text}")
+        except Exception as e:
+            print(f"Bir hata oluştu: {e}")
+
+def main():
+    # DISABLE etmek için:
+    #update_environment_state(ENV_IDS, CLUSTER_TOKEN, BASE_URL, "DISABLED")
+
+    # ENABLE etmek için (eğer gerekirse):
+     update_environment_state(ENV_IDS, CLUSTER_TOKEN, BASE_URL, "ENABLED")
+
+if __name__ == "__main__":
+    main()
